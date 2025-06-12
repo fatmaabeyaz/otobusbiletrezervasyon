8ffc0d9c381f3f3f23000cd5e9247d71d74f5cdd
Fatma <138524043+fatmaabeyaz@users.noreply.github.com>
2025-06-12 12:53:37 +0300

    Create Yolcu.cs

---
+++ Yolcu.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace otobusbilet
+{
+    public class Yolcu
+    {
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+        public string Telefon { get; set; }
+        public string Cinsiyet { get; set; }
+    }
+}
+
