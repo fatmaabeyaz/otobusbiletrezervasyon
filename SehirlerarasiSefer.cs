fe483fbff4e4daa75848ebcff3f233615d62df9b
Fatma <138524043+fatmaabeyaz@users.noreply.github.com>
2025-06-12 12:57:07 +0300

    Create SehirlerarasiSefer.cs

---
+++ SehirlerarasiSefer.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace otobusbilet
+{
+    public class SehirlerarasiSefer : Sefer, IRezervasyon
+    {
+        private decimal fiyat;
+
+        public SehirlerarasiSefer(string nereden, string nereye, DateTime tarih, Otobus otobus, decimal fiyat)
+            : base(nereden, nereye, tarih, otobus)
+        {
+            this.fiyat = fiyat;
+        }
+
+        public override decimal Fiyat
+        {
+            get { return fiyat; }
+            set { fiyat = value; }
+        }
+
+        public override void RezerveEt(Yolcu yolcu, Koltuk koltuk)
+        {
+            if (!koltuk.Rezerve)
+            {
+                koltuk.Rezerve = true;
+                koltuk.Yolcu = yolcu;
+            }
+        }
+
+        public void RezerveGoster()
+        {
+            foreach (var koltuk in Otobus.Koltuklar)
+            {
+                if (koltuk.Rezerve)
+                {
+                    Console.WriteLine($"Koltuk {koltuk.Numara} - {koltuk.Yolcu.Isim} {koltuk.Yolcu.Soyisim}");
+                }
+            }
+        }
+    }
+}
